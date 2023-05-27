38d4452b954542a2877f2f293e952cdac6d6921b
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2023-05-27 04:32:38 +0300

    turn off @Test annotation

--- src/test/java/com/project/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/com/project/telegram_bot/TelegramBotApplicationTests.java
@@ -1,12 +1,11 @@
 package com.project.telegram_bot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TelegramBotApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 

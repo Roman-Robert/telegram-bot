6d483352d4cd9a296c7a3304e559a8c3fcd2916f
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2023-06-01 16:50:28 +0300

    some adds

--- src/main/java/com/project/telegram_bot/model/UserRepository.java
+++ src/main/java/com/project/telegram_bot/model/UserRepository.java
@@ -2,10 +2,6 @@ package com.project.telegram_bot.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 
 public interface UserRepository extends CrudRepository <User, Long> {
-    public List<User> findUsersIsActiveYes();
-
 }
